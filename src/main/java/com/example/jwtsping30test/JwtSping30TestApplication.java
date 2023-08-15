e7837e3c5871053d4026ce08b006d50138c8059a
Nazarkep355 <Qu@antium3228>
2023-08-15 11:54:30 +0300

    Initial commit

---
+++ src/main/java/com/example/jwtsping30test/JwtSping30TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtsping30test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSping30TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSping30TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtsping30test/JwtSping30TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtsping30test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSping30TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
