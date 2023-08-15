a0f29396f7b9aa985c5e37d09f9757bc2618027c
Nazarkep355 <Qu@antium3228>
2023-08-15 13:40:42 +0300

    JwtService config step1

---
+++ src/main/java/com/example/jwtsping30test/config/JwtAuthFilter.java
@@ -0,0 +1,33 @@
+package com.example.jwtsping30test.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtService jwtService;
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if(authHeader==null||!authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+
+    }
+}
